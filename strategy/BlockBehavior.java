2235b28390b464d0ef5d8e6161e2ae1b1dcde582
Brendan McNichols <brendanmcnichols@Brendans-MacBook-Pro.local>
2024-01-14 06:39:37 -0500

    finished BlockBehavior class

--- strategy/BlockBehavior.java
+++ strategy/BlockBehavior.java
@@ -1,5 +1,15 @@
-package strategy;
+import java.util.Random;
+public class BlockBehavior implements DefenceBehavior {
+    public String play() {
+        Random random = new Random();
+        int RandomNumber = random.nextInt(3);
 
-public class BlockBehavior {
-    
+        if (randomNumber == 0) {
+            return "block player from passing";
+        } else if (randomNumber == 1) {
+            return "blocks player from shooting";
+        } else {
+            return "checks player with puck";
+        }
+    }
 }
