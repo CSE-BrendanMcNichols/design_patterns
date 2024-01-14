f8f42259b4645cd8cd31436ef435956f1f1ae1f1
Brendan McNichols <brendanmcnichols@Brendans-MacBook-Pro.local>
2024-01-14 11:21:20 -0500

    edited Player class

--- strategy/Player.java
+++ strategy/Player.java
@@ -1,23 +1,26 @@
 package strategy;
+import java.util.Random;
 public abstract class Player {
     protected String firstName;
     protected String lastName;
-    protected OffenceBehavior offenceBehavior;
     protected DefenceBehavior defenceBehavior;
+    protected OffenceBehavior offenceBehavior;
+    protected Random rand;
 
     public Player(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.rand = new Random();
     }
 
-    public abstract String play(boolean hasPossession);
+    public abstract String play(boolean possession);
 
-    public OffenceBehavior getOffenceBehavior() {
-        return offenceBehavior;
+    public void setDefenceBehavior(DefenceBehavior defenceBehavior) {
+        this.defenceBehavior = defenceBehavior;
     }
 
-    public DefenceBehavior getDefenceBehavior() {
-        return defenceBehavior;
+    public void setOffenceBehavior(OffenceBehavior offenceBehavior) {
+        this.offenceBehavior = offenceBehavior;
     }
 
     public String toString() {
