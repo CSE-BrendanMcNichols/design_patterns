b869fdf6cba0646a83ac0f358668a034db708539
Brendan McNichols <brendanmcnichols@Brendans-MacBook-Pro.local>
2024-01-14 07:01:00 -0500

    finished BlockGoalBehavior class

--- strategy/BlockGoalBehavior.java
+++ strategy/BlockGoalBehavior.java
@@ -1,5 +1,17 @@
 
 public class BlockGoalBehavior implements DefenceBehavior {
-    private String[] messages = {"hand blocks the puck"}
-    
+    public string Play() {
+        Random random = new Random(); 
+        int RandomNumber = new random.next(4);
+
+        if (randomNumber == 0) {
+            return "hand blocks the puck";
+        } else if (randomNumber == 1) {
+            return "catches the puck";
+        } else if (randomNumber == 2) {
+            return "blocks puck with knee pads";
+        } else {
+            return "blocks puck with stick";
+        }
+    }
 }
