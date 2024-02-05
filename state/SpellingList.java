fcae1ba5dc5dc048def9f0bea77e2f2e1e72ed98
CSE-BrendanMcNichols <mcnichob@email.sc.edu>
2024-02-04 23:01:46 -0500

    added java doc to all classes

--- state/SpellingList.java
+++ state/SpellingList.java
@@ -1,11 +1,16 @@
 package state;
-
+/**
+ * @author Brendan McNichols
+ * Manages the spelling list and transitions between different grades
+ */
 public class SpellingList {
     private State state;
     private FirstGradeState firstGradeState;
     private SecondGradeState secondGradeState;
     private ThirdGradeState thirdGradeState;
-
+/**
+* Initializes the spelling list and sets it to the first grade state
+*/
     public SpellingList() {
         firstGradeState = new FirstGradeState(this);
         secondGradeState = new SecondGradeState(this);
@@ -13,32 +18,46 @@ public class SpellingList {
         
         state = firstGradeState;
     }
-
+/**
+* @return The next word to spell
+*/
     public String getNextWord() {
         return state.getNextWord();
     }
-
+ /**
+* Increases the grade of the spelling list
+*/
     public void increaseGrade() {
         state.increaseGrade();
     }
-
+/**
+* Decreases the grade of the spelling list
+*/
     public void decreaseGrade() {
         state.decreaseGrade();
     }
-
+/**
+* @return The first grade state
+*/
     public State getFirstGradeState() {
         return firstGradeState;
     }
-
+/**
+* @return The second grade state
+*/
     public State getSecondGradeState() {
         return secondGradeState;
     }
-
+/**
+* @return The third grade state
+*/
     public State getThirdGradeState() {
         return thirdGradeState;
     }
-
-    public void setState(State newState) {
-        this.state = newState;
+/**
+* @param state state to set
+*/
+    public void setState(State state) {
+        this.state = state;
     }
 }
