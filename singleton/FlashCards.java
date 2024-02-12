c420d063be34fc3807ca05730ca8e36981aa0989
CSE-BrendanMcNichols <mcnichob@email.sc.edu>
2024-02-12 00:03:25 -0500

    removed unncessary comments

--- singleton/FlashCards.java
+++ singleton/FlashCards.java
@@ -10,13 +10,13 @@ public class FlashCards {
     private Random rand = new Random();
     private ArrayList<Word> words;
     private static FlashCards flashCardsInstance;
-    private Word currentWord; // Adding the currentWord field
+    private Word currentWord;
 
     private FlashCards() {
         words = FileReader.getWords();
         if (words.isEmpty()) {
             System.out.println("No words available.");
-            System.exit(1); // Exit the program as no words are available
+            System.exit(1);
         }
     }
 
@@ -39,12 +39,11 @@ public class FlashCards {
 */
     public Word getWord() {
         if (words.isEmpty()) {
-            // This should not happen as we exit if the list is empty during initialization
-            currentWord = new Word("No more words", "", "", ""); // Assign dummy Word to currentWord
+            currentWord = new Word("No more words", "", "", "");
             return currentWord;
         }
         int index = rand.nextInt(words.size());
-        currentWord = words.remove(index); // Assign the word to currentWord before removing it
+        currentWord = words.remove(index);
         return currentWord;
     }
 }
